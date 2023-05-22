b004c579e4069cf8d2a72a766c5a217cb14c015b
MohitShahu12 <Mohit@accrualify.com>
2023-05-23 05:25:21 +0530

    update neorg.lua working now

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -1,26 +1,14 @@
 return {
-  {
-    "nvim-neorg/neorg",
-    ft = { "norg" },
-    build = ":Neorg sync-parsers",
-    opts = {
+  "nvim-neorg/neorg",
+  ft = "norg", -- lazy load on filetype
+  cmd = "Neorg", -- lazy load on command, allows you to autocomplete :Neorg regardless of whether it's loaded yet
+  --  (you could also just remove both lazy loading things)
+  priority = 30, -- treesitter is on default priority of 50, neorg should load after it.
+  config = function()
+    require("neorg").setup({
       load = {
-        ["core.defaults"] = {}, -- Loads default behaviour
-        ["core.norg.concealer"] = {}, -- Adds pretty icons to your documents
-        ["core.norg.qol.toc"] = {},
-        ["core.norg.dirman"] = { -- Manages Neorg workspaces
-          config = {
-            workspaces = {
-              notes = vim.env.HOME .. "/norg-notes/",
-            },
-          },
-        },
-        ["core.presenter"] = {
-          config = {
-            zen_mode = "zen-mode",
-          },
-        },
+        ["core.defaults"] = {},
       },
-    },
-  },
+    })
+  end,
 }
